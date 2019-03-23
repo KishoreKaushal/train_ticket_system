d9922058f48da4429e34ade268e5d4c157a3469c
KishoreKaushal <kshr4kshl@gmail.com>
2019-03-24 01:11:49 +0530

    modified admin@localhost privileges

--- sql/grants.sql
+++ sql/grants.sql
@@ -1,2 +1,7 @@
 grant execute on procedure train_ticket_system.train_between_stations to rl_public;
 grant execute on procedure train_details to rl_public;
+
+grant create user on *.* TO rl_admin ;
+grant all privileges on train_ticket_system.* TO rl_admin with grant option;
+
+set default role rl_admin for admin@localhost;
--- sql/roles.sql
+++ sql/roles.sql
@@ -62,3 +62,27 @@ Query OK, 0 rows affected (0.000 sec)
 MariaDB [train_ticket_system]> set default role rl_public for usr_public@localhost;
 Query OK, 0 rows affected (0.020 sec)
 
+MariaDB [(none)]> create role rl_user;
+Query OK, 0 rows affected (0.000 sec)
+
+MariaDB [(none)]> show grants for rl_user;
++-----------------------------------------------------------+
+| Grants for rl_user                                        |
++-----------------------------------------------------------+
+| GRANT USAGE ON *.* TO 'rl_user'                           |
+| GRANT UPDATE ON `train_ticket_system`.`user` TO 'rl_user' |
++-----------------------------------------------------------+
+2 rows in set (0.000 sec)
+
+MariaDB [(none)]> show grants for admin@localhost;
++--------------------------------------------------------------------------------------------------------------+
+| Grants for admin@localhost                                                                                   |
++--------------------------------------------------------------------------------------------------------------+
+| GRANT rl_admin TO 'admin'@'localhost'                                                                        |
+| GRANT rl_user TO 'admin'@'localhost' WITH ADMIN OPTION                                                       |
+| GRANT USAGE ON *.* TO 'admin'@'localhost' IDENTIFIED BY PASSWORD '*4ACFE3202A5FF5CF467898FC58AAB1D615029441' |
+| GRANT ALL PRIVILEGES ON `mysql`.* TO 'admin'@'localhost'                                                     |
++--------------------------------------------------------------------------------------------------------------+
+4 rows in set (0.000 sec)
+
+
